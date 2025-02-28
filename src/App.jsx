1408b92a5a7ddcfb6413ee4cced62d5b7b98d68f
okechpj <okechpj@gmail.com>
2025-03-01 00:31:17 +0300

    added redirect pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';  // Import Router components
-import Button from '../components/Button';  // Import the Button component
-// import JoinGDG from './components/JoinGDG';  // Import page components
-// import Support from './components/Support';
-// import About from './components/About';
-// import ContactUs from './components/Contact_Us';  // Adjust the import name to match your file name
+import Button from './components/Button';  // Import the Button component
+import JoinGDG from './pages/JoinGDG';  // Import page components
+import Support from './pages/Support';
+import About from './pages/About';
+import ContactUs from './pages/Contact_Us';  // Adjust the import name to match your file name
 
 function App() {
   const [count, setCount] = useState(0);
@@ -13,16 +13,18 @@ function App() {
   return (
     <Router> {/* Wrapping everything with Router */}
       <div>
-        {/* Insert the Buttons component here */}
-        <Button />
+          {/* Insert the Buttons component here */}
+          <Button />
         {/* Define Routes */}
-        {/* Define routes for each page */}
-        {/* <Routes>
+        {/* Define   routes for each page */}
+        <Routes>
           <Route path="/join-gdg" element={<JoinGDG />} />
           <Route path="/support" element={<Support />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact-us" element={<ContactUs />} />
-        </Routes> */}
+        </Routes>
+
+        
 
         {/* Footer
         <footer>
